a58b529d10e36c6b5998d71cb72a28ebd47e1ff0
Otuekong Arthur <52894752+fizy500@users.noreply.github.com>
2022-10-28 19:07:03 +0100

    Done with profile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-import Links from "./Components/Links";
+import { Profile } from "./Components/Links";
+import { Links } from "./Components/Links";
+import { Logos } from "./Components/Links";
+import { Footer } from "./Components/Links";
+import data from "./data";
 export default function App() {
+  const links = data.map((item) => {
+    return <Links key={item.id} {...item} />;
+  });
   return (
-    <div className="grid grid-rows-5 h-full ">
-      <Links />
+    <div className="my-4 ">
+      <Profile/>
+      {links}
+      <Logos />
+      <Footer />
     </div>
   );
 }
--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,59 +1,73 @@
 import React from "react";
-export default function Links() {
+import Avatar from "../Assets/avatar.svg";
+import Sharemobile from "../Assets/sharemobile.svg";
+import ShareDesk from "../Assets/sharedesktop.svg";
+import Slack from "../Assets/slack.svg";
+import Github from "../Assets/github.svg";
+import Zuri from "../Assets/Zuri.Internship_Logo.svg";
+import I4G from "../Assets/I4G.svg";
+
+export function Profile() {
   return (
-    <section className=" w-full   px-3 py-5">
-      <div className="grid grid-rows-6 space-y-3 text-center ">
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900">Twitter Link</p>
-          <p className="">Description</p>
-        </a>
-
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900">Zuri Team</p>
-          <p className="">Description</p>
-        </a>
-
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900">Zuri Books</p>
-          <p className="">Description</p>
-        </a>
-
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900">Python Books</p>
-          <p className="">Description</p>
-        </a>
-
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900">
-            {" "}
-            Background Check for Coders
-          </p>
-          <p className="">Description</p>
-        </a>
-
-        <a
-          href=""
-          className="bg-gray-200 px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center "
-        >
-          <p className="text-lg font-medium text-gray-900"> Design Books</p>
-          <p className="">Description</p>
-        </a>
+    <section className="  flex justify-center items-center  mx-8 relativeb ">
+      <div className="flex flex-col justify-center items-center">
+        <img src={Avatar} alt="" className="w-20 h-20 rounded-[50%]" />
+        <h2 className="font-bold text-gray-900 text-lg md:text-xl">
+          Otuekong Idongesit
+        </h2>
       </div>
+      <a href="">
+        <img
+          src={ShareDesk}
+          alt=""
+          className="hidden md:block  absolute top-3 right-[30rem] w-10 h-10 rounded-[50%] border-zinc-100 border-dotted border-2"
+        />
+      </a>
+      <a href="">
+        <img
+          src={Sharemobile}
+          alt=""
+          className="block md:hidden  absolute top-3 right-5"
+        />
+      </a>
     </section>
+  );
+}
+
+export function Links(props) {
+  return (
+    <a
+      href={props.link}
+      className="bg-gray-200 text-center px-20 py-8 rounded-md hover:bg-gray-400  flex flex-col justify-center items-center mx-8 my-4 "
+      id={props.identifier}
+    >
+      <p className="text-sm md:text-lg font-medium text-gray-900">
+        {props.title}
+      </p>
+      <p className="">{props.description}</p>
+    </a>
+  );
+}
+
+export function Logos() {
+  return (
+    <div className="flex justify-center items-center space-x-5 pb-8">
+      <img src={Slack} alt="" />
+      <img src={Github} alt="" />
+    </div>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer className="w-full  px-5 md:grid grid-cols-1 justify-center items-center py-10">
+      <div className="space-y-4 md:space-y-0 md:flex justify-between items-center py-4 border-t-2 border-gray-200">
+        <img src={Zuri} alt="" />
+        <p className="text-sm md:text-base text-gray-500 font-normal">
+          HNG Internship 9 Frontend Task
+        </p>
+        <img src={I4G} alt="" />
+      </div>
+    </footer>
   );
 }
