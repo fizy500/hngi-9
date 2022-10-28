d38ac89b7ffedbd9862712bb6dbd43ce1ec14c86
Otuekong Arthur <52894752+fizy500@users.noreply.github.com>
2022-10-28 14:20:16 +0100

    Added props to the Links component.

---
+++ src/Components/data.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    id: 1,
+    identifier: "",
+    title: "Twitter Link",
+    description: "",
+    link: "https://twitter.com/Fizy_hector",
+  },
+
+  {
+    id: 2,
+    identifier: "btn__zuri",
+    title: "Zuri Team",
+    description: "",
+    link: "https://training.zuri.team/",
+  },
+
+  {
+    id: 3,
+    identifier: "books",
+    title: "Zuri Books",
+    description: "",
+    link: "http://books.zuri.team ",
+  },
+
+  {
+    id: 4,
+    identifier: "book__python",
+    title: "Python Books",
+    description: "",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=<fizy_hector>",
+  },
+
+  {
+    id: 5,
+    identifier: "pitch",
+    title: "Background Check for Coders",
+    description: "",
+    link: "https://background.zuri.team",
+  },
+  {
+    id: 6,
+    identifier: "book__design",
+    title: "Design Books",
+    description: "",
+    link: "https://books.zuri.team/design-rules",
+  },
+];
