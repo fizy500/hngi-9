ee91d54636f75fa17243f38a32d360b948edbcf0
Otuekong Arthur <52894752+fizy500@users.noreply.github.com>
2022-11-05 13:44:48 +0100

    fix:Fixed the link component being rendered as an empty link on empty link on homepage.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 import Profile from "../Components/Profile";
 import Links from "../Components/Links";
-import Logos from "../Components/Links";
+import Logos from "../Components/Logos";
 import Footer from "../Components/Footer";
 import data from "../data";
 export default function Home() {
   const links = data.map((item) => {
     return <Links key={item.id} {...item} />;
   });
-  console.log(links);
   return (
     <div className="my-6 ">
       <Profile />
