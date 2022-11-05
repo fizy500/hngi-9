02463af423f5b8a686115c355f84753c3e9b1b94
Otuekong Arthur <52894752+fizy500@users.noreply.github.com>
2022-11-05 23:56:35 +0100

    refactor:Cleaned the app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
 import Home from "./Pages/Home";
 export default function App() {
   return (
     <div>
       <Home />
-      {/* <Routes>
-          <Route path="/hngi-9" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes> */}
     </div>
   );
 }
